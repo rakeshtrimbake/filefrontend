ae4e70904ee29039b56357750367280fd038de01
unknown <trimbake.rakesh1@gmail.com>
2020-10-11 18:07:52 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,24 @@ import axios from "axios";
 function App() {
 
   const [selectFile,setSelectFile] = useState('');
-  const onClickHandler = () => {
+  const [size,setSize] = useState(0);
+  const [sha1,setSha1] = useState('');
+  const onClickHandler = async () => {
          const data = new FormData()
          data.append('file', selectFile)
          console.log(data)
-         axios.post("http://localhost:4000/upload", data, {
-           })
-           .then(res => {
-           alert("file uploaded successfully");
-           })
+        const result = await axios.post("http://localhost:4000/upload", data)
+       
+        setSize(result.data.fileSize);
+        setSha1(result.data.sha1_format);
+           
      }
 
   return (
     <div className="app">
       <p class="form-group files">
         <label>Upload Your File </label>
-     
+  {size > 0 ? <p>Your file size is ${size} and sha1 is ${sha1}</p>:null}
         <input type="file" name="file" onChange={(e) => setSelectFile(e.target.files[0])} />
       </p>
     
