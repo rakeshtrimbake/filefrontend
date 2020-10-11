59825b625cee0ab2c4be9a6812c89105b61887a3
unknown <trimbake.rakesh1@gmail.com>
2020-10-11 09:44:43 +0530

    remov commented code

--- src/App.js
+++ src/App.js
@@ -36,47 +36,5 @@ function App() {
 }
 
 export default App;
-// class App extends Component {
-//   constructor(props) {
-//     super(props);
-//       this.state = {
-//         selectFile: null
-//       }
 
-//   }
-//   onChangeHandler=event=>{
-//     this.setState({
-//       selectFile: event.target.files[0],
-//       loaded: 0,
-//     })
-//   }
-
-//   onClickHandler = () => {
-//     const data = new FormData()
-//     data.append('file', this.state.selectFile)
-//     console.log(data)
-//     axios.post("http://localhost:4000/upload", data, {
-//       })
-//       .then(res => {
-//        // console.log(res.statusText)
-//       })
-// }
-
-//   render(){
-//     return (
-//       <React.Fragment>
-//             <div class="row">
-
-//             <p class="form-group files">
-//               <label>Upload Your File </label>
-//               <input type="file" name="file" onChange={this.onChangeHandler}/>
-//             </p>
-//             </div>
-//             <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button>
-//             </React.Fragment>
-
-// );
-//   }
-
-// }
 
